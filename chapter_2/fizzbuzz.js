33d31b9507f36037cffeac9b917a7997d1751f21
hanaum <hanaum@github.com>
2016-06-04 21:39:50 -0700

    Ch2.fizzbuzz

---
+++ chapter_2/fizzbuzz.js
@@ -0,0 +1,19 @@
+function fizzbuzz() {
+  for (var i = 1; i <= 100; i++) {
+    var output = '';
+    if (i % 3 === 0) {
+      output += 'Fizz';
+    }
+    if (i % 5 === 0) {
+      output += 'Buzz';
+    }
+
+    if (output.length === 0) {
+      output = i.toString();
+    }
+
+    console.log(output);
+  }
+}
+
+fizzbuzz();
