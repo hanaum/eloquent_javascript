2e894d48a35e8cd1b94c0b51933e2ae33d43b4a5
hanaum <hanau12@gmail.com>
2016-06-09 14:01:11 -0700

    Ch4.sum. Ch4.expandRange.

--- chapter_4/range.js
+++ chapter_4/range.js
@@ -1,28 +1,46 @@
-function range(start, end) {
-	if (start === end) {
-		return [start];
-	} else if (start < end) {
-		return increasingRange(start, end);
-	} else {
-		return decreasingRange(start, end);
-	}
+function range(start, end, step) {
+  if (start === end) {
+    return [start];
+  } else if (start < end) {
+    return increasingRange(start, end, step);
+  } else {
+    return decreasingRange(start, end, step);
+  }
 }
 
-function increasingRange(start, end) {
-	var range = [];
-	for (var i = start; i <= end; i++) {
-		range.push(i);
-	}
-	return range;
+function increasingRange(start, end, step = 1) {
+  var range = [];
+  if (step < 0) {
+    return [];
+  }
+  for (var i = start; i <= end; i += step) {
+    range.push(i);
+  }
+  return range;
 }
 
-function decreasingRange(start, end) {
-	var range = [];
-	for (var i = start; i >= end; i--) {
-		range.push(i);
-	}
-	return range;
+function decreasingRange(start, end, step = -1) {
+  var range = [];
+  if (step > 0) {
+    return [];
+  }
+  for (var i = start; i >= end; i += step) {
+    range.push(i);
+  }
+  return range;
+}
+//------------------------------------------
+
+function sum(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+  return array.reduce(function(total, num) {
+    return total + num;
+  });
 }
 
-console.log(range(-10, 10));
-console.log(range(10, 1));
+console.log(range(10, -10, -3));
+console.log(range(-10, 10, 2));
+console.log(sum(range(1, 10)));
+console.log(sum([]));
