8f99a7fd1239bbd6611f4a5df6b0a8d58c0da64d
hanaum <hanau12@gmail.com>
2016-06-13 14:22:10 -0700

    Ch5.everySome

---
+++ chapter_5/everySome.js
@@ -0,0 +1,23 @@
+function every(arr, elem) {
+	for (var i = 0; i < arr.length; i++) {
+		if (!elem(arr[i])) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function some(arr, elem) {
+	for (var i = 0; i < arr.length; i++) {
+		if (elem(arr[i])) {
+			return true;
+		}
+	}
+	return false;
+}
+
+// test cases
+console.log(every([NaN, NaN, NaN], isNaN));
+console.log(every([NaN, NaN, 4], isNaN));
+console.log(some([NaN, 3, 4], isNaN));
+console.log(some([2, 3, 4], isNaN));
