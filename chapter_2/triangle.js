2a2a49e93425353df8f02f1e820e0045865f8811
hanaum <hanau12@gmail.com>
2016-06-12 19:07:24 -0700

    Ch2.triangle

---
+++ chapter_2/triangle.js
@@ -0,0 +1,15 @@
+function triangle(num) {
+	var str = '';
+	if (num === 0) {
+		return str;
+	}
+	for (var i = 1; i < num + 1; i++) {
+		for (var j = 1; j < i + 1; j++) {
+			str += '#';
+		}
+		str += '\n';
+	}
+	return str;
+}
+console.log(triangle(0));
+console.log(triangle(14));
