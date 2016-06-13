4f4bb6a889c6ca6042eed2dda21cacaf6ec2a7a8
hanaum <hanau12@gmail.com>
2016-06-12 18:17:06 -0700

    Fix deepCopy.

--- chapter_4/deepCopy.js
+++ chapter_4/deepCopy.js
@@ -1,6 +1,4 @@
 function deepEqual(obj1, obj2) {
-  console.log(obj1);
-  console.log(obj2);
   if (typeof obj1 !== typeof obj2) {
     return false;
   }
@@ -9,23 +7,19 @@ function deepEqual(obj1, obj2) {
   }
   var obj1PropertyNames = Object.getOwnPropertyNames(obj1);
   if (obj1PropertyNames.length !== Object.getOwnPropertyNames(obj2).length) {
-    console.log('here');
     return false;
   }
   if (obj1PropertyNames.length === 0) {
     return true;
   }
 
-  for (var prop in obj1PropertyNames) {
+  for (var i = 0; i < obj1PropertyNames.length; i++) {
+    var prop = obj1PropertyNames[i];
     if (obj2.hasOwnProperty(prop)) {
       if (!deepEqual(obj1[prop], obj2[prop])) {
-            console.log('here1');
         return false;
       }
     } else {
-      console.log(prop);
-          console.log('here2');
-
       return false;
     }
   }
@@ -33,11 +27,13 @@ function deepEqual(obj1, obj2) {
   return true;
 }
 
+// test cases
 var obj1 = {
   a: 1,
   b: 2,
   c: {
-    d: 3
+    d: 3,
+    e: 0
   }
 }
 
@@ -45,7 +41,8 @@ var obj2 = {
   a: 1,
   b: 2,
   c: {
-    d: 3
+    d: 3,
+    e: 0
   }
 }
 
